f8b46171b07d23faab23e6caf0df020d53838799
Prashant Choudhary <choudhary.prashant9795@gmail.com>
2020-07-27 11:21:48 +0530

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude,longitude, callback) => {
         }else{
             callback(undefined, body.current.weather_descriptions[0] +
                             '. It is currently '+ body.current.temperature +
-                         ' degrees and feels like '+ body.current.feelslike + ' degrees out')
+                         ' degrees and feels like '+ body.current.feelslike + ' degrees out.')
         }
     })
 
